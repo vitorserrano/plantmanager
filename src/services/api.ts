7ff5eaef8c5a68aae732ffbaf1cffd01e6a0f940
vitorserrano <vitorcirinoserrano@gmail.com>
2021-04-26 19:14:46 -0300

    fix: change api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.17:3333',
+  baseURL: process.env.API_URL,
 });
 
 export default api;
-
-// PC - 'http://192.168.31.174:3333',
-// Notebook - 'http://192.168.15.17:3333',
-
-// npm install -g json-server
-// json-server ./src/services/server.json --host 192.168.31.174 --port 3333
